a964d102da52c993c95cb594858ef4a0341da8ce
Reggie <reggie.senal@gmail.com>
2024-04-30 13:56:33 +0800

    init commit

---
+++ demoservice1/src/main/java/com/microservices/service1/demoservice1/Demoservice1Application.java
@@ -0,0 +1,12 @@
+package com.microservices.service1.demoservice1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoservice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demoservice1Application.class, args);
+	}
+}
---
+++ demoservice1/src/test/java/com/microservices/service1/demoservice1/Demoservice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.service1.demoservice1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demoservice1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

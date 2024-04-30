b640d7709d0115be01b1c1dff81b839263d63d29
Reggie <reggie.senal@gmail.com>
2024-04-30 18:10:43 +0800

    interact service 1 with service 2

--- demoservice1/src/main/java/com/microservices/service/controller/Service1Controller.java
+++ demoservice1/src/main/java/com/microservices/service/controller/Service1Controller.java
@@ -1,7 +1,9 @@
 package com.microservices.service.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import com.microservices.service.model.Profile;
 import com.microservices.service.model.User;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +15,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class Service1Controller {
 
     @GetMapping("/user/{id}")
-    public User getUser(@PathVariable("id") String id){
+    public User getUser(@PathVariable("id") String id) {
         User user = new User(Integer.parseInt(id), "John Doe", "8090090293");
+        RestTemplate restTemplate = new RestTemplate();
+        Profile profile = restTemplate.getForObject("http://localhost:9001/service/profile/1", Profile.class);
+        user.setProfile(profile);
         return user;
     }
 }
--- demoservice1/src/main/java/com/microservices/service/model/Profile.java
+++ demoservice1/src/main/java/com/microservices/service/model/Profile.java
@@ -1,8 +1,40 @@
 package com.microservices.service.model;
 
+import java.util.List;
+
 public class Profile {
 
-    public Profile() {
+    private int id;
+    private List<String> posts;
+    private List<String> notifications;
 
+    public Profile(int id, List<String> posts, List<String> notifications) {
+        this.id = id;
+        this.posts = posts;
+        this.notifications = notifications;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public List<String> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<String> posts) {
+        this.posts = posts;
+    }
+
+    public List<String> getNotifications() {
+        return notifications;
+    }
+
+    public void setNotifications(List<String> notifications) {
+        this.notifications = notifications;
     }
 }
